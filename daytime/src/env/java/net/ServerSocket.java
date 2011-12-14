3079107e99e5027d09325679064f9aabbbce508e
Yuto Takei <TakeiYuto+github@gmail.com>
2011-12-14 23:53:41 +0900

    Exercise 3.

---
+++ daytime/src/env/java/net/ServerSocket.java
@@ -0,0 +1,18 @@
+/* $Id: ServerSocket.java 838 2010-11-24 08:16:23Z cartho $ */
+
+package env.java.net;
+
+/* ServerSocket stub. */
+
+import gov.nasa.jpf.jvm.Verify;
+import java.io.IOException;
+
+public class ServerSocket {
+  
+  public ServerSocket(int port) throws IOException {
+  }
+
+  public Socket accept() throws IOException {
+    return new Socket();
+  }
+}
